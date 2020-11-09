4908f186baf36295f4cc28fdddbfb7d32a5dc167
mr-caoducanh <mr.caoducanh@gmail.com>
2020-11-09 23:11:03 +0700

    Update start window

---
+++ src/main/aca/oop/BombermanACAGame.java
@@ -0,0 +1,10 @@
+package main.aca.oop;
+
+import main.aca.oop.gui.GUI;
+//import main.aca.oop.exceptions.BombermanException;
+
+public class BombermanACAGame {
+    public static void main(String[] args) {
+        new GUI().setVisible(true);
+    }
+}
---
+++ src/main/aca/oop/gui/GUI.java
@@ -0,0 +1,32 @@
+package main.aca.oop.gui;
+
+import java.awt.CardLayout;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+
+public class GUI extends JFrame{
+    public static final int WIDTHJF = 905;
+    public static final int HEIGHTJF = 610;
+    private MyContainer mContainer;
+
+    public GUI() {
+        setSize(WIDTHJF, HEIGHTJF);
+        setLayout(new CardLayout());
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        mContainer = new MyContainer(this);
+        add(mContainer);
+        addWindowListener(mwindow);
+    }
+
+    private WindowAdapter mwindow = new WindowAdapter() {
+        @Override
+        public void windowClosing(WindowEvent e) {
+            //GameSound stop
+            //PlayGame.IS_RUNNING = false;
+        }
+    };
+}
